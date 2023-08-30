cd794d3515aac95476c09d3e25c41b9901ea60f8
tbw875 <charger875@gmail.com>
2023-08-30 13:37:00 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,95 @@
+// Load environment variables from .env file
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const { Client } = require("pg");
+
+const app = express();
+
+// Setting up DB connection
+
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: process.env.DB_PASSWORD,
+  database: "screen-db",
+});
+
+client.connect();
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Enable JSON body parsing
+
+// ROUTE: post request
+app.post("/api/entities", async (req, res) => {
+  const { address } = req.body;
+  const apiEndpoint = "https://api.chainalysis.com/api/risk/v2/entities";
+  try {
+    const response = await axios.post(
+      apiEndpoint,
+      { address },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Token: process.env.SERVER_API_KEY,
+        },
+      }
+    );
+
+    // Send response back to frontend
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error calling external API", error);
+    res.status(500).send("Internal Server Error", error); // Sending error response to client
+  }
+});
+
+// ROUTE: get request
+app.get("/api/entities/:address", async (req, res) => {
+  const { address } = req.params; // Captured from the URL
+  const apiEndpoint = `https://api.chainalysis.com/api/risk/v2/entities/${address}`;
+
+  console.log(`Server GET request for address: ${address}`); // Logging address for debugging
+
+  try {
+    const response = await axios.get(apiEndpoint, {
+      headers: {
+        "Content-Type": "application/json",
+        Token: process.env.SERVER_API_KEY,
+      },
+    });
+
+    // Send response back to frontend
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error calling external API", error);
+    res.status(500).send("Internal Server Error"); // Sending error response to client
+  }
+});
+
+// ROUTE: sql insert
+app.post("/api/insert", async (req, res) => {
+  try {
+    const { address, risk } = req.body;
+    const data = req.body; // this is the entire JSON object
+
+    // Insert data to db
+    const query = `
+        INSERT INTO responses (address, risk, data)
+        VALUES ($1, $2, $3);`;
+
+    await client.query(query, [address, risk, data]);
+    console.log("Data successfully inserted to db");
+    res.status(200).json({ message: "Data stored in database" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server running on http://localhost:3001");
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
+  const [address, setAddress] = useState("");
+  const [responseData, setResponseData] = useState(null);
+
+  const handlePostRequest = async () => {
+    const apiEndpoint = "http://localhost:3001/api/entities";
+    console.log("API Key:", process.env.REACT_APP_API_KEY);
+    console.log("Address:", address);
+    try {
+      const response = await axios.post(
+        apiEndpoint,
+        {
+          address: address,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Token: process.env.REACT_APP_API_KEY,
+          },
+        }
+      );
+
+      if (response.status === 200 && response.data.address) {
+        toast.success(
+          `POST request was successful for address: ${response.data.address}`
+        );
+      } else {
+        toast.error(
+          "POST request was successful, but unexpected response format."
+        );
+      }
+    } catch (error) {
+      if (error.response) {
+        toast.error(`Error in POST request: ${error.response.status}`);
+        console.log(error);
+      } else {
+        // The request was made but no response was received or an error occurred setting up the request
+        toast.error(`Error in POST request: ${error.message}`);
+        console.log(error);
+      }
+    }
+  };
+
+  const handleGetRequest = async () => {
+    const apiEndpoint = `http://localhost:3001/api/entities/${address}`;
+    try {
+      console.log(`Making GET request to ${apiEndpoint}`);
+      const response = await axios.get(apiEndpoint, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      setResponseData(response.data);
+      toast.success("GET request was successful!");
+    } catch (error) {
+      if (error.response) {
+        toast.error(`Error in GET request: ${error.response.status}`);
+        console.log(error.response.data);
+      } else {
+        // The request was made but no response was received or an error occurred setting up the request
+        toast.error(`Error in GET request: ${error.message}`);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Welcome to Cryptocurrency Address Checker</h1>
+      <div>
+        <label>
+          Enter Address:
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </label>
+        <button onClick={handlePostRequest}>POST</button>
+        <button onClick={handleGetRequest}>GET</button>
+      </div>
+
+      {responseData && (
+        <pre className="json-response">
+          {JSON.stringify(responseData, null, 2)}
+        </pre>
+      )}
+
+      <ToastContainer />
     </div>
   );
 }
