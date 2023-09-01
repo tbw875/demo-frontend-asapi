30da95725970491e438ffd420758f840721f2192
tbw875 <charger875@gmail.com>
2023-09-01 10:44:37 -0700

    add thirdweb connect button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
+
 function App() {
   const [address, setAddress] = useState("");
   const [responseData, setResponseData] = useState(null);
@@ -86,29 +94,38 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1>Welcome to Cryptocurrency Address Checker</h1>
-      <div>
-        <label>
-          Enter Address:
-          <input
-            type="text"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </label>
-        <button onClick={handlePostRequest}>POST</button>
-        <button onClick={handleGetRequest}>GET</button>
+    <ThirdwebProvider
+      activeChain="polygon"
+      clientId="da67985796103cccc170eb42e81de46b"
+      supportedWallets={[metamaskWallet(), coinbaseWallet(), walletConnect()]}
+    >
+      <div className="container">
+        <h1>Welcome to Cryptocurrency Address Checker</h1>
+
+        <ConnectWallet theme={"dark"} />
+
+        <div>
+          <label>
+            Enter Address:
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </label>
+          <button onClick={handlePostRequest}>POST</button>
+          <button onClick={handleGetRequest}>GET</button>
+        </div>
+
+        {responseData && (
+          <pre className="json-response">
+            {JSON.stringify(responseData, null, 2)}
+          </pre>
+        )}
+
+        <ToastContainer />
       </div>
-
-      {responseData && (
-        <pre className="json-response">
-          {JSON.stringify(responseData, null, 2)}
-        </pre>
-      )}
-
-      <ToastContainer />
-    </div>
+    </ThirdwebProvider>
   );
 }
 
